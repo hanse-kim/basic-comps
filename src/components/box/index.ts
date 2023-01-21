c9466482cf4898218f872f9c7197f99b5bbc3b47
bluewood-truth <bluewood.truth@gmail.com>
2023-01-21 11:13:42 +0900

    split props and use array for performance
    - Using array increases render duration 3~5%.

--- src/components/box/index.ts
+++ src/components/box/index.ts
@@ -1,8 +1,6 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import {
-  AnimationProps,
-  animationStyles,
   BackgroundProps,
   backgroundStyles,
   BorderProps,
@@ -11,46 +9,37 @@ import {
   effectStyles,
   FlexboxProps,
   flexboxStyles,
-  GridItemProps,
-  gridItemStyles,
-  GridProps,
-  gridStyles,
   LayoutProps,
   layoutStyles,
   ParagraphProps,
   paragraphStyles,
-  PositionProps,
-  positionStyles,
   SpaceProps,
   spaceStyles,
-  TransitionProps,
-  transitionStyles,
 } from 'src/core/styles';
 
-export type BoxProps = AnimationProps &
-  BackgroundProps &
+export type BoxProps = BackgroundProps &
   BorderProps &
   EffectProps &
   FlexboxProps &
-  GridProps &
-  GridItemProps &
   LayoutProps &
   ParagraphProps &
-  PositionProps &
-  SpaceProps &
-  TransitionProps;
+  SpaceProps;
 
 export const Box = styled.div<BoxProps>`
-  ${(props) => animationStyles(props)}
-  ${(props) => backgroundStyles(props)}
-  ${(props) => borderStyles(props)}
-  ${(props) => effectStyles(props)}
-  ${(props) => flexboxStyles(props)}
-  ${(props) => gridStyles(props)}
-  ${(props) => gridItemStyles(props)}
-  ${(props) => layoutStyles(props)}
-  ${(props) => paragraphStyles(props)}
-  ${(props) => positionStyles(props)}
-  ${(props) => spaceStyles(props)}
-  ${(props) => transitionStyles(props)}
+  ${(props) => [
+    defaultBoxStyles,
+    backgroundStyles(props),
+    borderStyles(props),
+    effectStyles(props),
+    flexboxStyles(props),
+    layoutStyles(props),
+    paragraphStyles(props),
+    spaceStyles(props),
+  ]}
 `;
+
+const defaultBoxStyles = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+});
