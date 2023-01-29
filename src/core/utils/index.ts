0cdb9d925ef081de2bd0ce143fac15d311b292d7
bluewood-truth <bluewood.truth@gmail.com>
2023-01-29 17:20:41 +0900

    fix emotion styled export

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,7 +1,7 @@
-export * as styled from '@emotion/styled';
 export { parseColor } from './parse-color';
 export { parseFontWeight } from './parse-font-weight';
 export { parseSize } from './parse-size';
 export { parseSpace } from './parse-space';
 export { parseZIndex } from './parse-z-index';
+export * from './styled';
 export * from './_common';
