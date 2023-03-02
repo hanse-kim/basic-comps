ee192ce6ae52c4d772ea974ee26b82b747b9b747
hanse <bluewood.truth@gmail.com>
2023-03-03 00:18:55 +0900

    build: add rollup-plugin-dts to bundle index.d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,38 @@
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'es',
+export default [
+  {
+    input: 'src/components/index.ts',
+    output: {
+      dir: 'dist',
+      sourcemap: true,
+    },
+
+    plugins: [
+      copy({
+        targets: [{ src: './package.json', dest: './dist' }],
+      }),
+      typescript(),
+      terser(),
+    ],
+    external: [/react.+/, /@emotion.+/],
   },
-  plugins: [
-    copy({ targets: [{ src: './package.json', dest: './dist' }] }),
-    typescript(),
-    terser(),
-  ],
-  external: [/react.+/, /@emotion.+/],
-};
+  {
+    input: 'src/components/index.ts',
+    output: {
+      dir: 'dist',
+      format: 'es',
+    },
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: '.',
+        },
+      }),
+    ],
+    external: [/react.+/, /@emotion.+/],
+  },
+];
