102fc8a6e3d8d18b894799c89d970a193b4f0072
hanse <bluewood.truth@gmail.com>
2023-03-06 22:14:02 +0900

    build: remove code splitting

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,13 @@
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import fs from 'fs';
-import path from 'path';
-
-const __dirname = path.resolve();
-
-const getFiles = (folderPath) => {
-  return fs.readdirSync(folderPath).flatMap((fileName) => {
-    const filePath = path.join(folderPath, fileName);
-    if (fs.statSync(filePath).isDirectory()) {
-      return getFiles(filePath);
-    } else {
-      return {
-        fileName: filePath
-          .replace(path.join(__dirname, 'src/'), '')
-          .replace('.ts', ''),
-        filePath,
-      };
-    }
-  });
-};
-
-const inputFiles = getFiles(path.join(__dirname, 'src'));
-console.log(inputFiles);
 
 export default {
-  input: Object.fromEntries(
-    inputFiles
-      .filter(({ filePath }) => filePath.endsWith('index.ts'))
-      .map(({ fileName, filePath }) => [fileName, filePath])
-  ),
+  input: 'src/components/index.ts',
   output: [
     {
       dir: 'dist',
       format: 'esm',
+      sourcemap: true,
     },
   ],
   plugins: [typescript(), terser()],
