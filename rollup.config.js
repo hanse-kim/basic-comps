d634584542994fbaa89dd8ae57d4637a6f04db48
hanse <bluewood.truth@gmail.com>
2023-03-07 21:57:28 +0900

    build: setup to bundling type declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/components/index.ts',
-  output: [
-    {
-      dir: 'dist',
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript(), terser()],
-  external: [/react.+/, /@emotion.+/],
-};
+export default [
+  {
+    input: 'src/components/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript(), terser()],
+    external: [/react.+/, /@emotion.+/],
+  },
+  {
+    input: 'src/components/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts({ compilerOptions: { baseUrl: '.' } })],
+  },
+];
