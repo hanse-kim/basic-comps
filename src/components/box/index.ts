68b85c58321258e5aa25d821316759b2c6c9865f
bluewood-truth <bluewood.truth@gmail.com>
2024-01-16 21:55:21 +0900

    add box component

---
+++ src/components/box/index.ts
@@ -0,0 +1,54 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import {
+  AnimationProps,
+  animationStyles,
+  BackgroundProps,
+  backgroundStyles,
+  BorderProps,
+  borderStyles,
+  EffectProps,
+  effectStyles,
+  FlexboxProps,
+  flexboxStyles,
+  GridItemProps,
+  gridItemStyles,
+  GridProps,
+  gridStyles,
+  LayoutProps,
+  layoutStyles,
+  ParagraphProps,
+  PositionProps,
+  positionStyles,
+  SpaceProps,
+  spaceStyles,
+  TransitionProps,
+  transitionStyles,
+} from 'src/core/styles';
+
+export type BoxProps = AnimationProps &
+  BackgroundProps &
+  BorderProps &
+  EffectProps &
+  FlexboxProps &
+  GridProps &
+  GridItemProps &
+  LayoutProps &
+  ParagraphProps &
+  PositionProps &
+  SpaceProps &
+  TransitionProps;
+
+export const Box = styled.div<BoxProps>`
+  ${(props) => animationStyles(props)}
+  ${(props) => backgroundStyles(props)}
+  ${(props) => borderStyles(props)}
+  ${(props) => effectStyles(props)}
+  ${(props) => flexboxStyles(props)}
+  ${(props) => gridStyles(props)}
+  ${(props) => gridItemStyles(props)}
+  ${(props) => layoutStyles(props)}
+  ${(props) => positionStyles(props)}
+  ${(props) => spaceStyles(props)}
+  ${(props) => transitionStyles(props)}
+`;
